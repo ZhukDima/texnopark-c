15f6c5b352d4b5e69ae12edf0ec753d391b291e2
ZhukDima <zhukdo@gmail.com>
2020-10-13 23:15:33 +0300

    Test #2

--- build/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user1/texnopark-c/build/googletest-src
-# Build directory: /home/user1/texnopark-c/build/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/googletest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user1/texnopark-c/build/googletest-src/googletest
-# Build directory: /home/user1/texnopark-c/build/googletest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
