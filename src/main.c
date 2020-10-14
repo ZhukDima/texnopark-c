862eea1db4df4e47e7d5a4f834ae88d8ebe3fcda
Vadim Khizhnyakov <provcydia@gmail.com>
2020-10-15 00:33:29 +0300

    Main function was modified. Added console input and output

--- src/main.c
+++ src/main.c
@@ -3,7 +3,33 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello world!\n");
+  printf("Введите размер массива\n");
+  int size = 0;
+	scanf("%d", &size);
+	int* arr = malloc(size * sizeof(int));
+	int* dist;
+	//int arr2[] = { 1, 2, 3, -6, 9, 10, 11, 12, 13, 14 };
+	
+  printf("Введите сам массив\n");
+	for (int i = 0; i < size; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
 
+  printf("Введенный массив\n");
+	for (int i = 0; i < size; i++)
+	{
+    printf("%d ", arr[i]);
+	}
+  printf("\n");
+
+	int dist_size = arithmeticProgInArray(arr, &dist, size);
+
+	printf("Максимальная последовательность, являющаяся арифметической прогрессией\n");
+	for (int i = 0; i < dist_size; i++)
+	{
+    printf("%d ", dist[i]);
+	}
+  printf("\n");
   return 0;
 }
